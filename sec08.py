441bbe7d9f17f04f3a7541ba61d9bb8a7191a5a2
eunbum <icesnake72@gmail.com>
2023-05-04 16:06:46 +0900

    thread를 이용한 구글 이미지 크롤링

--- sec08.py
+++ sec08.py
@@ -1,27 +1,29 @@
-import requests
+import requests, time
 from bs4 import BeautifulSoup
 
-url = "https://www.google.com/search?q=python&tbm=isch&ved=2ahUKEwjR7K3x19j-AhV6U_UHHeUTAq8Q2-cCegQIABAA&oq=python&gs_lcp=CgNpbWcQAzIECCMQJzIECCMQJzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEUABYAGDJEGgAcAB4AIABdIgBdJIBAzAuMZgBAKoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=VxVSZJHlMvqm1e8P5aeI-Ao&bih=854&biw=1504&hl=ko"
-userAgent = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
-response = requests.get(url, headers=userAgent)
-with open('yt.html', 'wt') as f:
-  f.write(response.text)
-with open('yt.html', 'rt') as f:
-  soup = BeautifulSoup(f, "html.parser")
-  imgs = soup.find_all('img') #, attrs={'data-src'})
-  # print(len(imgs))
-  tot = 0
-  for img in imgs:
-    try:
-      # print(img['data-src'])
-      res = requests.get(img['data-src'])
-      if res:
+start = time.time()
+
+url = "https://www.google.com/search?q=korea&rlz=1C5CHFA_enKR1010KR1010&sxsrf=APwXEddiTIk2v25rMaUTASedf74K_d92qQ:1683176769403&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjYwryL8tr-AhXYEXAKHeeaCucQ_AUoAXoECAIQAw&biw=1504&bih=1556&dpr=2"
+header = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+response = requests.get(url, headers={"User-Agent":header})
+
+soup = BeautifulSoup(response.content, "html.parser")
+imgs = soup.find_all('img', attrs={'data-src':True})
+
+tot = 1
+for img in imgs:
+  try:
+    # print(img['data-src'])
+    res = requests.get(img['data-src'])
+    if res.status_code==200:      
+      with open(f'result_{tot}.jpg', 'wb') as f:
+        f.write(res.content)
         tot += 1
-        with open(f'result_{tot}.jpg', 'wb') as f:
-          f.write(res.content)
-    except: continue
-    
-  print(tot)
+  except: continue
+
+end = time.time()
+print(f'프로그램 종료 : {end-start}')  
+
       # print(img['src'])
     
     
@@ -29,3 +31,26 @@ with open('yt.html', 'rt') as f:
 
 # for thumbnail in thumbnails:
 #     print(thumbnail["src"])
+# class A():
+#   def __init__(self):
+#     super().__init__()
+#     self.x = 1
+
+# # parent class B
+# class B():
+#   def __init__(self):
+#     super().__init__()
+#     self.y = 2
+
+# # parent class C
+# class C():
+#   def __init__(self):
+#     self.z = 3
+
+# # target class D
+# class D(A, B, C):
+#   def __init__(self):
+#     super().__init__()
+
+# d = D()
+# print(vars(d))
